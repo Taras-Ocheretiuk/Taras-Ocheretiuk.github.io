9c76ff0b1571f69a9c597b320fe96b78733001f8
Taras Ocheretiuk <150180428+Taras-Ocheretiuk@users.noreply.github.com>
2024-12-22 18:18:56 +0200

    Add files via upload
    add 22.12

--- Travellian/script/script.js
+++ Travellian/script/script.js
@@ -8,3 +8,15 @@ burgerIcons.addEventListener('click',() => {
     openIcon.classList.toggle('hide')
     closeIcon.classList.toggle('hide')
 })
+
+
+const destinationsSwiper = new Swiper('.swiper-destinations', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-destinations .btn-swiper-right',
+        prevEl: '.swiper-destinations .btn-swiper-left',
+    },
+    autoplay: {
+        delay: 3000,
+    },
+});
